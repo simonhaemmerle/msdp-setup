49d9b47bc07976f6f063d3fa87e3b214cfe2e493
Simon Hämmerle <simon.haemmerle@ionos.com>
2023-11-09 10:24:51 +0100

    Initial commit with Terraform resources

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# msdp-setup
+# MSDP Setup
+
+## Preparations for Terraform (locally)
+
+Before starting it is recommeded to have [ionosctl](https://github.com/ionos-cloud/ionosctl#getting-started) running on your machine.
+
+You also need to make sure that the user you plan to use for terraforming is either a contract owner, an administrator or a users with the Manage Dataplatform permission (as documented [here](https://docs.ionos.com/cloud/managed-services/managed-stackable/how-tos/initial-cluster-setup)).
+
+Then run `ionosctl login` to login with your credentials and `ionosctl token generate` to retrieve a token.
+
+Now we need to set two environment variables. The variable `TF_VAR_cluster_description` let's Terraform know how the file is called in which we stored our cluster configurations. 
+
+```shell
+export TF_VAR_cluster_description=cluster-test.yaml
+```
+
+The variable `TF_VAR_ionos_token` should contain our token for the authentication against the IONOS API.
+
+```shell
+export TF_VAR_ionos_token=<yourToken>
+```
+
+>**Info:** If you plan to run Terraform in a pipeline instead of locally you would store the variales `TF_VAR_cluster_description` and `TF_VAR_ionos_token` in your Gitlab or Github variables in your repo.
+
+## Run Terraform (locally)
+
+Before running the Terraform command you need to navigate to the `terraform` folder.
+
+```shell
+cd terraform
+```
+
+Then run your typical Terraform commands.
+
+```shell
+terraform init
+terraform validate
+terraform plan
+terraform apply
+```
+
+For destroying your cluster again use this command:
+
+```shell
+terraform destroy
+```
+
+>**Info:** You can run the same commands in the same order in your Gitlab pipeline or Github action and maybe separate them into different stages / jobs.
---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  # https://docs.ionos.com/terraform-provider/api/data-platform
+  # https://registry.terraform.io/providers/ionos-cloud/ionoscloud/latest
+  required_providers {
+    ionoscloud = {
+      source = "ionos-cloud/ionoscloud"
+      version = "6.4.10"
+    }
+  }
+}
+
+# We use token authentication or use TF_VAR_ionos_username + TF_VAR_ionos_password
+# Generate an [Authentication](https://api.ionos.com/docs/authentication/#authentication) token here or use ionosctl (https://docs.ionos.com/cli-ionosctl/subcommands/authentication/token/generate)
+variable "ionos_token" {
+  description = "Token to be used with the IONOS Cloud Provider - set using environment variable TF_VAR_ionos_token"
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_description" {
+  description = "Name of the cluster description file - set using environment variable TF_VAR_cluster_description"
+  type        = string
+}
+
+# https://registry.terraform.io/providers/ionos-cloud/ionoscloud/latest/docs
+provider "ionoscloud" {
+  token = var.ionos_token
+}
+
+# https://registry.terraform.io/providers/ionos-cloud/ionoscloud/latest/docs/resources/datacenter
+resource "ionoscloud_datacenter" "datacenter" {
+  name = yamldecode(file("${var.cluster_description}"))["spec"]["name"]
+  location = yamldecode(file("${var.cluster_description}"))["spec"]["location"]
+  description = "Datacenter containing nodes for cluster ${yamldecode(file("${var.cluster_description}"))["spec"]["name"]}"
+}
+
+# https://registry.terraform.io/providers/ionos-cloud/ionoscloud/latest/docs/resources/dataplatform_cluster
+resource "ionoscloud_dataplatform_cluster" "cluster" {
+  datacenter_id           =  ionoscloud_datacenter.datacenter.id
+  name                     = yamldecode(file("${var.cluster_description}"))["spec"]["name"]
+  maintenance_window {
+    day_of_the_week      = "Sunday"
+    time                = "09:00:00"
+  }
+  version = yamldecode(file("${var.cluster_description}"))["spec"]["stackable_version"]
+}
+
+# https://registry.terraform.io/providers/ionos-cloud/ionoscloud/latest/docs/resources/dataplatform_node_pool
+resource "ionoscloud_dataplatform_node_pool" "nodepool" {
+  cluster_id        = ionoscloud_dataplatform_cluster.cluster.id
+  name              = yamldecode(file("${var.cluster_description}"))["spec"]["name"]
+  node_count        = yamldecode(file("${var.cluster_description}"))["spec"]["node_count"]
+  cpu_family        = yamldecode(file("${var.cluster_description}"))["spec"]["cpu_family"]
+  cores_count       = 8
+  ram_size          = 16384
+  availability_zone = "AUTO"
+  storage_type      = "SSD"
+  storage_size      = 100
+  maintenance_window {
+    day_of_the_week = "Monday"
+    time            = "09:00:00"
+  }
+}
